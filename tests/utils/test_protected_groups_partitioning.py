285c1c71edad0759618789d8f42d4d8400e6f188
Denys Herasymuk <herasymuk@ucu.edu.ua>
2023-03-19 18:59:53 +0200

    Fixed unit tests

---
+++ tests/utils/test_protected_groups_partitioning.py
@@ -0,0 +1,80 @@
+from sklearn.model_selection import train_test_split
+from virny.utils.protected_groups_partitioning import check_sensitive_attrs_in_columns, create_test_protected_groups
+
+from tests import config_params, compas_dataset_class, compas_without_sensitive_attrs_dataset_class
+
+
+def test_check_sensitive_attrs_in_columns_true(config_params):
+    df_columns = ['age', 'juv_fel_count', 'juv_misd_count', 'juv_other_count', 'priors_count',
+                  'race', 'age_cat_25 - 45', 'age_cat_Greater than 45',
+                  'age_cat_Less than 25', 'c_charge_degree_F', 'c_charge_degree_M', 'sex']
+    actual = check_sensitive_attrs_in_columns(df_columns,
+                                              config_params.sensitive_attributes_dct)
+    assert actual == True
+
+
+def test_check_sensitive_attrs_in_columns_false(config_params):
+    df_columns = ['age', 'juv_fel_count', 'juv_misd_count', 'juv_other_count', 'priors_count',
+                  'race', 'age_cat_25 - 45', 'age_cat_Greater than 45',
+                  'age_cat_Less than 25', 'c_charge_degree_F', 'c_charge_degree_M']
+    actual = check_sensitive_attrs_in_columns(df_columns,
+                                              config_params.sensitive_attributes_dct)
+    assert actual == False
+
+
+def test_create_test_protected_groups_true(compas_dataset_class, config_params):
+    seed = 42
+    X_train, X_test, y_train, y_test = train_test_split(compas_dataset_class.X_data,
+                                                        compas_dataset_class.y_data,
+                                                        test_size=config_params.test_set_fraction,
+                                                        random_state=seed)
+    actual_test_protected_groups = create_test_protected_groups(X_test, compas_dataset_class.full_df,
+                                                                config_params.sensitive_attributes_dct)
+
+    assert len(actual_test_protected_groups) == len(config_params.sensitive_attributes_dct.keys()) * 2
+
+    assert actual_test_protected_groups['sex_priv'].shape[0] == 211
+    assert actual_test_protected_groups['sex_dis'].shape[0] == 845
+    assert actual_test_protected_groups['race_priv'].shape[0] == 414
+    assert actual_test_protected_groups['race_dis'].shape[0] == 642
+    assert actual_test_protected_groups['sex&race_priv'].shape[0] == 99
+    assert actual_test_protected_groups['sex&race_dis'].shape[0] == 530
+
+
+def test_create_test_protected_groups_true_without_sensitive_attrs(compas_without_sensitive_attrs_dataset_class, config_params):
+    seed = 42
+    X_train, X_test, y_train, y_test = train_test_split(compas_without_sensitive_attrs_dataset_class.X_data,
+                                                        compas_without_sensitive_attrs_dataset_class.y_data,
+                                                        test_size=config_params.test_set_fraction,
+                                                        random_state=seed)
+    actual_test_protected_groups = create_test_protected_groups(X_test, compas_without_sensitive_attrs_dataset_class.full_df,
+                                                                config_params.sensitive_attributes_dct)
+
+    assert len(actual_test_protected_groups) == len(config_params.sensitive_attributes_dct.keys()) * 2
+
+    assert actual_test_protected_groups['sex_priv'].shape[0] == 211
+    assert actual_test_protected_groups['sex_dis'].shape[0] == 845
+    assert actual_test_protected_groups['race_priv'].shape[0] == 414
+    assert actual_test_protected_groups['race_dis'].shape[0] == 642
+    assert actual_test_protected_groups['sex&race_priv'].shape[0] == 99
+    assert actual_test_protected_groups['sex&race_dis'].shape[0] == 530
+
+
+def test_create_test_protected_groups_true2(compas_without_sensitive_attrs_dataset_class, config_params):
+    new_sensitive_attributes_dct = {'sex': 0, 'race': 'Caucasian'}
+
+    seed = 42
+    X_train, X_test, y_train, y_test = train_test_split(compas_without_sensitive_attrs_dataset_class.X_data,
+                                                        compas_without_sensitive_attrs_dataset_class.y_data,
+                                                        test_size=config_params.test_set_fraction,
+                                                        random_state=seed)
+    actual_test_protected_groups = create_test_protected_groups(X_test,
+                                                                compas_without_sensitive_attrs_dataset_class.full_df,
+                                                                new_sensitive_attributes_dct)
+
+    assert len(actual_test_protected_groups) == len(new_sensitive_attributes_dct.keys()) * 2
+
+    assert actual_test_protected_groups['sex_priv'].shape[0] == 211
+    assert actual_test_protected_groups['sex_dis'].shape[0] == 845
+    assert actual_test_protected_groups['race_priv'].shape[0] == 414
+    assert actual_test_protected_groups['race_dis'].shape[0] == 642
--- tests/utils/test_stability_utils.py
+++ tests/utils/test_stability_utils.py
@@ -1,8 +1,12 @@
 import numpy as np
 
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
+
 from tests import config_params, compas_dataset_class, compas_without_sensitive_attrs_dataset_class
 from virny.utils.stability_utils import count_prediction_stats, generate_bootstrap
-from virny.utils.custom_initializers import create_base_pipeline
+from virny.preprocessing.basic_preprocessing import preprocess_dataset
 
 
 # ========================== Test count_prediction_stats ==========================
@@ -39,13 +43,17 @@ def test_count_prediction_stats_true2():
 
 # ========================== Test generate_bootstrap ==========================
 def test_generate_bootstrap_true1(compas_without_sensitive_attrs_dataset_class, config_params):
-    base_pipeline = create_base_pipeline(compas_without_sensitive_attrs_dataset_class,
-                                         config_params.sensitive_attributes_dct,
-                                         model_seed=config_params.runs_seed_lst[0],
-                                         test_set_fraction=config_params.test_set_fraction)
-    boostrap_size = int(config_params.bootstrap_fraction * base_pipeline.X_train_val.shape[0])
-    X_sample, y_sample = generate_bootstrap(base_pipeline.X_train_val,
-                                            base_pipeline.y_train_val,
+    column_transformer = ColumnTransformer(transformers=[
+        ('categorical_features', OneHotEncoder(handle_unknown='ignore', sparse=False), compas_without_sensitive_attrs_dataset_class.categorical_columns),
+        ('numerical_features', StandardScaler(), compas_without_sensitive_attrs_dataset_class.numerical_columns),
+    ])
+    base_flow_ds = preprocess_dataset(compas_without_sensitive_attrs_dataset_class,
+                                      column_transformer,
+                                      test_set_fraction=0.2,
+                                      dataset_split_seed=42)
+    boostrap_size = int(config_params.bootstrap_fraction * base_flow_ds.X_train_val.shape[0])
+    X_sample, y_sample = generate_bootstrap(base_flow_ds.X_train_val,
+                                            base_flow_ds.y_train_val,
                                             boostrap_size,
                                             with_replacement=True)
 
