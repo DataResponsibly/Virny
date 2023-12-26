2bcdab8f7095a3076637e2354883bd5275c778f4
denysgerasymuk799 <herasymuk@ucu.edu.ua>
2023-12-27 00:03:18 +0200

    Added StudentPerformancePortugueseDataset

--- virny/datasets/data_loaders.py
+++ virny/datasets/data_loaders.py
@@ -9,7 +9,6 @@ from virny.datasets.base import BaseDataLoader
 
 
 class CreditCardDefaultDataset(BaseDataLoader):
-
     def __init__(self, dataset_path: str = None):
         if dataset_path is None:
             filename = 'credit_card_default_clean.csv'
@@ -66,6 +65,46 @@ class CreditDataset(BaseDataLoader):
                              'DebtRatio', 'MonthlyIncome', 'NumberOfOpenCreditLinesAndLoans',
                              'NumberOfTimes90DaysLate', 'NumberRealEstateLoansOrLines']
         categorical_columns = []
+
+        super().__init__(
+            full_df=df,
+            target=target,
+            numerical_columns=numerical_columns,
+            categorical_columns=categorical_columns,
+        )
+
+
+class StudentPerformancePortugueseDataset(BaseDataLoader):
+    """
+    Dataset class for the Student Performance Portuguese dataset that contains sensitive attributes among feature columns.
+    Source: https://github.com/tailequy/fairness_dataset/blob/main/experiments/data/student_por_new.csv
+    Description: https://arxiv.org/pdf/2110.00530.pdf (Section 3.4.1)
+
+    Parameters
+    ----------
+    subsample_size
+        Subsample size to create based on the input dataset
+    subsample_seed
+        Seed for sampling using the sample() method from pandas
+
+    """
+    def __init__(self, subsample_size: int = None, subsample_seed: int = None):
+        filename = 'student_por_new.csv'
+        dataset_path = pathlib.Path(__file__).parent.joinpath(filename)
+        df = pd.read_csv(dataset_path)
+
+        if subsample_size:
+            df = df.sample(subsample_size, random_state=subsample_seed) if subsample_seed is not None \
+                else df.sample(subsample_size)
+            df = df.reset_index(drop=True)
+
+        # Preprocessing
+        df['class'] = [1 if v == "High" else 0 for v in df['class']]
+
+        target = 'class'
+        numerical_columns = ['age', 'Medu', 'Fedu', 'traveltime', 'studytime', 'failures', 'famrel', 'freetime',
+                             'goout', 'Dalc', 'Walc', 'health', 'absences', 'G1', 'G2']
+        categorical_columns = [column for column in df.column if column not in numerical_columns + [target]]
 
         super().__init__(
             full_df=df,
