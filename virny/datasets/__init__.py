f829d4f80d6cc59695145ec1c31ec3816e93214e
denysgerasymuk799 <herasymuk@ucu.edu.ua>
2024-03-11 17:29:01 +0200

    Separated folktables data loaders

--- virny/datasets/__init__.py
+++ virny/datasets/__init__.py
@@ -2,9 +2,10 @@
 This module contains sample datasets and data loaders.
 The purpose is to provide sample datasets for functionality testing and show examples of data loaders (aka dataset classes).
 """
-from .data_loaders import CompasWithoutSensitiveAttrsDataset, DiabetesDataset, CompasDataset, \
-    ACSIncomeDataset, ACSEmploymentDataset, ACSMobilityDataset, ACSTravelTimeDataset, ACSPublicCoverageDataset, \
-    RicciDataset, LawSchoolDataset, CreditCardDefaultDataset, StudentPerformancePortugueseDataset
+from .data_loaders import (CompasWithoutSensitiveAttrsDataset, DiabetesDataset, CompasDataset,
+                           RicciDataset, LawSchoolDataset, CreditCardDefaultDataset, StudentPerformancePortugueseDataset)
+from .folktables import (ACSIncomeDataset, ACSEmploymentDataset, ACSMobilityDataset, ACSTravelTimeDataset,
+                         ACSPublicCoverageDataset)
 
 
 __all__ = [
